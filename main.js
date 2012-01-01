202aa8a62b8c8299df9dc842a0050e3901774721
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:36:41 -0300

    cambio: se movio todo del archivo newAmount.js a main.js

--- main.js
+++ main.js
@@ -3,3 +3,25 @@ import Account from './account.js';
 
 const defaultAccount = new Account('default')
 defaultAccount.firstInit()
+
+const newAmountForm = document.getElementById('newAmountForm')
+newAmountForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newAmount = getNewAmountFromForm()
+    cleanForm()
+    defaultAccount.logAmount(newAmount)
+})
+const amountInput = document.getElementById('amountInput')
+const amountTypeSelect = document.getElementById('amountTypeSelect')
+const commentInput = document.getElementById('commentInput')
+const getNewAmountFromForm = () => ({
+    id: 1,
+    amount: amountInput.value,
+    type: amountTypeSelect.value,
+    comment: commentInput.value
+})
+const cleanForm = () => {
+    amountInput.value = ''
+    amountTypeSelect.value = 'none'
+    commentInput.value = ''
+}
