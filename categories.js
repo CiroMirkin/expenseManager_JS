5143ad374e3606ee06296ec4a80ded714232dcb7
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:41:15 -0300

    agregado: metodo que retorna un input html con las categorias de ingresos

--- categories.js
+++ categories.js
@@ -8,7 +8,19 @@ export default class Categories {
             }
         ]
         this.expenseCategories = [
-            
+
         ]
     }
+    getSelectInputOfIncomeCategories() {
+        return `
+            <select id="amountCategorieSelect" class="form-select mb-2">
+            <option value="none">...</option>
+            ${
+                this.incomeCategories.map(incomeCategorie => {
+                    return `<option value="${incomeCategorie.name}">${incomeCategorie.name}</option>`
+                }).join('')
+            }
+            </select>
+        `
+    } 
 }
