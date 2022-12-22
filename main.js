64ce9ccb6e19b6aa58536e445e0851baa4d8e303
C.Mirkin <ciromirkin@gmail.com>
2022-12-22 12:29:18 -0300

    agregado: se valida una entrada, pero no se hace nada con ella.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 "use strict"
+
+const addAmountSubmitInputBtn = document.getElementById('addAmountSubmitInputBtn')
+const amountInput = document.getElementById('amountInput')
+const amountTypeSelect = document.getElementById('amountTypeSelect')
+const commentInput = document.getElementById('commentInput')
+
+addAmountSubmitInputBtn.addEventListener('click', e => {
+    e.preventDefault()
+    if(isTheInputValid()) {
+        const newAmountForRegister = {
+            amount: amountInput.value.trim(),
+            type: amountTypeSelect.value,
+            commment: commentInput.value.trim()
+        }
+        cleanInputs()
+    }
+})
+
+const isTheInputValid = () => {
+    return !!amountInput.value.trim() && !!commentInput.value.trim() && amountTypeSelect.value !== 'none'
+}
+
+const cleanInputs = () => {
+    amountInput.value = ''
+    amountTypeSelect.value = ''
+    commentInput.value = 'none'
+}
