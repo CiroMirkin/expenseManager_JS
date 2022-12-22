a3aacd4bd7290797dc5f59bdd80f327588c932e9
C.Mirkin <ciromirkin@gmail.com>
2022-12-22 12:52:47 -0300

    agregado: forma de visualizar un monto

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const addAmountSubmitInputBtn = document.getElementById('addAmountSubmitInputBtn
 const amountInput = document.getElementById('amountInput')
 const amountTypeSelect = document.getElementById('amountTypeSelect')
 const commentInput = document.getElementById('commentInput')
+const amountListElement = document.getElementById('amountList')
 
 const defaultAccount = new Account('default')
 
@@ -24,11 +25,12 @@ addAmountSubmitInputBtn.addEventListener('click', e => {
     e.preventDefault()
     if(isTheInputValid()) {
         const newAmountForRegister = {
-            amount: amountInput.value.trim(),
+            amount: amountInput.value,
             type: amountTypeSelect.value,
-            commment: commentInput.value.trim()
+            comment: commentInput.value
         }
         defaultAccount.logAmount(newAmountForRegister)
+        showNewAmount(defaultAccount)
         cleanInputs()
     }
 })
@@ -41,4 +43,31 @@ const cleanInputs = () => {
     amountInput.value = ''
     amountTypeSelect.value = 'none'
     commentInput.value = ''
+}
+
+const showAllOfAmounts = (account) => {
+    const allOfAmounts = account.getAllOfAmounts()
+    amountListElement.innerHTML =  allOfAmounts.map(amount =>`
+        <li class="amountList__amount">
+            <div>
+                ${amount.amount}
+                <span>${amount.type}</span>
+            </div>
+            <p>${amount.comment}</p>
+        </li>
+    ` ).join('')
+}
+
+const showNewAmount = (account) => {
+    const allOfAmounts = account.getAllOfAmounts()
+    const newAmount = allOfAmounts.at(-1)
+    amountListElement.innerHTML +=  `
+        <li class="amountList__amount">
+            <div>
+                ${newAmount.amount}
+                <span>${newAmount.type}</span>
+            </div>
+            <p>${newAmount.comment}</p>
+        </li>
+    `
 }
