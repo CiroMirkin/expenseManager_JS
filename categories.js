c1dd01c4ce7a95e8c2b6acdefaf4be4b290586ea
C.mirkin <ciromirkin@gmail.com>
2023-01-28 10:47:30 -0300

    cambio: select input vacio para categorias movido desde la clase Categories hasta el modal para añadir un nuevo monto

--- categories.js
+++ categories.js
@@ -21,26 +21,22 @@ export default class Categories {
     }
     getSelectInputOfIncomeCategories() {
         return `
-            <select id="amountCategorieSelect" class="form-select mb-2">
             <option value="none">...</option>
             ${
                 this.incomeCategories.map(incomeCategorie => {
                     return `<option value="${incomeCategorie.name}">${incomeCategorie.name}</option>`
                 }).join('')
             }
-            </select>
         `
     } 
     getSelectInputOfExpenseCategories() {
         return `
-            <select id="amountCategorieSelect" class="form-select mb-2">
             <option value="none">...</option>
             ${
                 this.expenseCategories.map(expenseCategorie => {
                     return `<option value="${expenseCategorie.name}">${expenseCategorie.name}</option>`
                 }).join('')
             }
-            </select>
         `
     } 
 }
