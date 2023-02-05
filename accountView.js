f7fbdd55b4a413981ee17237dc43f093596ddb83
C.mirkin <ciromirkin@gmail.com>
2023-02-05 09:33:06 -0300

    agregado: cambio dinamico del grafico

--- accountView.js
+++ accountView.js
@@ -1,34 +1,12 @@
 export default class AccountView {
-    showListOfAmounts(list) {
-        const amountListElement = document.getElementById('amountList')
-        amountListElement.innerHTML = list.map(amount =>`
-            <li id="${amount.id}" class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="d-flex align-items-center">
-                <div class="py-1 px-2 d-flex justify-content-center align-items-center border border-secondary rounded">${amount.categorie.HTMLIcon}</div>
-                <div class="h5 mx-2 my-0">${amount.amount}</div>
-                </div>
-                <div>
-                    <button class="btn btn-primary" amount-action="edit"><i class="bi bi-pencil"></i></button>
-                    <button class="btn btn-danger" amount-action="delete"><i class="bi bi-trash3"></i></button>
-                </div>
-            </li>
-        ` ).join('')
-    }
-    showAmountTotalAccount(total) {
-        const acoountValuesHTMLElement = document.getElementById('accountValues')
-        acoountValuesHTMLElement.innerHTML = `<ul class="mx-3  rounded-2 border border-1 border-light-subtle border-opacity-50">
-            <li><span class="fs-6 opacity-50">Total</span><div class="fs-1">${total}</div></li>
-        </ul>`
-    }
-    showInChart(categories, valueOfCategories) {
-        const ctx = document.getElementById('chart');
-        new Chart(ctx, {
+    constructor() {
+        this.chart = new Chart(document.getElementById('chart'), {
             type: 'doughnut',
             data: {
-                labels: [...categories],
+                labels: ['', ''],
                 datasets: [
                     {
-                        data: [...valueOfCategories],
+                        data: [0, 0],
                         backgroundColor: [
                             'rgb(255, 99, 132)',
                             'rgb(54, 162, 235)',
@@ -55,6 +33,32 @@ export default class AccountView {
                   ctx.restore();
                 }
               }],
-        });
+        })
+    }
+    showListOfAmounts(list) {
+        const amountListElement = document.getElementById('amountList')
+        amountListElement.innerHTML = list.map(amount =>`
+            <li id="${amount.id}" class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="d-flex align-items-center">
+                <div class="py-1 px-2 d-flex justify-content-center align-items-center border border-secondary rounded">${amount.categorie.HTMLIcon}</div>
+                <div class="h5 mx-2 my-0">${amount.amount}</div>
+                </div>
+                <div>
+                    <button class="btn btn-primary" amount-action="edit"><i class="bi bi-pencil"></i></button>
+                    <button class="btn btn-danger" amount-action="delete"><i class="bi bi-trash3"></i></button>
+                </div>
+            </li>
+        ` ).join('')
+    }
+    showAmountTotalAccount(total) {
+        const acoountValuesHTMLElement = document.getElementById('accountValues')
+        acoountValuesHTMLElement.innerHTML = `<ul class="mx-3  rounded-2 border border-1 border-light-subtle border-opacity-50">
+            <li><span class="fs-6 opacity-50">Total</span><div class="fs-1">${total}</div></li>
+        </ul>`
+    }
+    showInChart(categories, valueOfCategories) {
+        this.chart.data.datasets[0].data = [...valueOfCategories]
+        this.chart.data.labels = [...categories]
+        this.chart.update()
     }
 }
